d6bd816ac5fc305d89a0ccb6794d7d6f3c84453c
Kirsten Nordine <kirstennordine@gmail.com>
2013-03-31 00:34:16 -0400

    Show up/down arrows for ingredients on hover

--- app/assets/javascripts/ingredients.js
+++ app/assets/javascripts/ingredients.js
@@ -2,10 +2,21 @@ $(document).ready(ingredientHandlers);
 
 function ingredientHandlers() {
   $('.overlay').click(pinIngredient);
+  $('.ingredient').hover(showArrows, hideArrows);
 }
 
 function pinIngredient() {
   if ($(this).parent().parent().hasClass("home") == false) {
     $(this).parent().toggleClass('pinned');
   }
+}
+
+function showArrows() {
+  $(this).find('.up-arrow').show();
+  $(this).find('.down-arrow').show();
+}
+
+function hideArrows() {
+  $(this).find('.up-arrow').hide();
+  $(this).find('.down-arrow').hide();
 }
