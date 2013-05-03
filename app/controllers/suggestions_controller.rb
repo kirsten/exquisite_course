e3f56b628c4d4df16a676c9eac7b57a53ace5351
Kirsten Nordine <kirstennordine@gmail.com>
2013-05-02 23:07:29 -0400

    Add a suggestion model

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,20 +1,12 @@
 class SuggestionsController < ApplicationController
-  def new
-    @starch = Group.find_by_id(1).ingredients.sample
-    @veggie = Group.find_by_id(2).ingredients.sample
-    @flair  = Group.find_by_id(3).ingredients.sample
-  end
 
-  def shuffle
+  def random
     @starch = Group.find_by_id(1).ingredients.sample
     @veggie = Group.find_by_id(2).ingredients.sample
     @flair  = Group.find_by_id(3).ingredients.sample
   end
 
   def show
-    @suggestion = Suggestion.all.sample
-    @starch = @suggestion.ingredients[0]
-    @veggie = @suggestion.ingredients[1]
-    @flair = @suggestion.ingredients[2]
+    @suggestion = Suggestion.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ ExquisiteCourse::Application.routes.draw do
   root :to => 'home#index'
   
   resources :suggestions, :only => :show
-  match '/shuffle', to: 'suggestions#shuffle'
+  match '/random', to: 'suggestions#random'
   resources :groups, :only => [:index]
 
   match '/about', to: 'static_pages#about'
