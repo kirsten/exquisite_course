6b978baa346c91bee73426c6d7957b311f36fb21
Kirsten Nordine <kirstennordine@gmail.com>
2013-06-16 14:00:32 -0400

    Suggestion model includes methods for groups, isn't AR

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -1,5 +1,29 @@
-class Suggestion < ActiveRecord::Base
-  attr_accessible :ingredient_ids
+class Suggestion
+  def self.find(ids)
+    ids = ids.split(",") #=> [1,2,3]
+    new(grain_id: ids[0], vegetable_id: ids[1], flair_id: ids[2])
+  end
 
-  has_and_belongs_to_many :ingredients
+  def initialize(ids)
+    @grain_id = ids[:grain_id]
+    @vegetable_id = ids[:vegetable_id]
+    @flair_id = ids[:flair_id]
+  end
+
+  def to_param
+    [@grain_id, @vegetable_id, @flair_id].join(",")
+  end
+
+  def grain
+    Ingredient.find(@grain_id)
+  end
+
+  def vegetable
+    Ingredient.find(@vegetable_id)
+  end
+
+  def flair
+    Ingredient.find(@flair_id)
+  end
+
 end
