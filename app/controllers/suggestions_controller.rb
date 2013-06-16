6c90bd23ba810cbc4dc47a8846098c43463b6668
Kirsten Nordine <kirstennordine@gmail.com>
2013-06-16 14:01:32 -0400

    SuggestionsController redirects random request to show page

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,12 +1,22 @@
 class SuggestionsController < ApplicationController
 
   def random
-    @starch = Group.find_by_id(1).ingredients.sample
-    @veggie = Group.find_by_id(2).ingredients.sample
-    @flair  = Group.find_by_id(3).ingredients.sample
+    @starch = random_ingredient(1)
+    @veggie = random_ingredient(2)
+    @flair  = random_ingredient(3)
+    suggestion = Suggestion.new(grain_id: @starch.id, vegetable_id: @veggie.id, flair_id: @flair.id)
+    redirect_to suggestion_path(suggestion)
   end
 
   def show
     @suggestion = Suggestion.find(params[:id])
+    @starch = @suggestion.grain
+    @veggie = @suggestion.vegetable
+    @flair = @suggestion.flair
   end
+
+  def random_ingredient(group_id)
+    IngredientFinder.new(group_id: group_id).random
+  end
+
 end
